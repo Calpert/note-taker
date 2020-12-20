06f14f8f6a43ac6c4de13eef56fa7055e9dafeef
Claudia Alpert <claudiaalpert@gmail.com>
2020-12-20 12:25:29 -0600

    deleted *

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 router.get('/notes', (req, res)=>{
     res.sendfile(path.join(__dirname, '../public/notes.html'))
 })
-router.get('/*', (req, res)=>{
+router.get('/', (req, res)=>{
     res.sendfile(path.join(__dirname, '../public/index.html'))
 })
 module.exports = router;
